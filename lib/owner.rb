072c3c8ac382078d8d8c49addaf95227ad21aff7
Ayako Wiltshire <awittyl44@gmail.com>
2019-12-30 14:51:45 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -55,12 +55,9 @@ class Owner
   end
    
   def sell_pets
-   pets.each do |species, animals|
-		 animals.each do |animal|
-		 	animal.mood = "nervous"
-		 end
-		 animals.clear
-		end 
+    @pets
+    binding.pry
+   
   end
   
   def list_pets
